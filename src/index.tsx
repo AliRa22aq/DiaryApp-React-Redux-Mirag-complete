98598020a0dfe7ea3dcd49398683aa29c11c0f63
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-12-08 18:17:50 +0500

    Changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
 import { setupServer } from './services/mirage/server';
+import { Provider } from 'react-redux';
+import {store} from './store'
 
 if (process.env.NODE_ENV === 'development') {
   setupServer();
@@ -12,7 +13,9 @@ if (process.env.NODE_ENV === 'development') {
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}> 
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -20,4 +23,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
